83783272697ae9e52c95a519be5586f2fa5c286d
Julian Garnier <hello@julian.gr>
2016-03-14 00:05:01 +0100

    remove 'Or'

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ window.onload = function() {
     'I\'m Julian',
     'I design and code things on the web',
     'I\'m currently accepting freelance work.<br> You can contact me at <a href="mailto:hello@julian.gr">hello@julian.gr</a>',
-    'Or<br><a target="_blank" href="https://twitter.com/juliangarnier">twitter.com/juliangarnier</a><br><a target="_blank" href="https://codepen.io/juliangarnier">codepen.io/juliangarnier</a><br><a target="_blank" href="https://github.com/juliangarnier">github.com/juliangarnier</a>',
+    '<a target="_blank" href="https://twitter.com/juliangarnier">twitter.com/juliangarnier</a><br><a target="_blank" href="https://codepen.io/juliangarnier">codepen.io/juliangarnier</a><br><a target="_blank" href="https://github.com/juliangarnier">github.com/juliangarnier</a>',
     getCurrentTime(),
     '👀 J.'
   ]
